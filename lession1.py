9b2b1a9103eba71f8369bde69e50dfd1de7f4bbd
jlslee <57722784+jlslee@users.noreply.github.com>
2019-11-18 12:42:31 -0800

    Create lession1.py

---
+++ lession1.py
@@ -0,0 +1,11 @@
+
+# set variable
+subject = "Hallmark Labs' Lunch and Learn!"
+
+user_name = input ("Tell me your name!!! ")
+
+welcome_message = "Welcome " + user_name + ' to ' + subject
+
+print (welcome_message)
+
+
